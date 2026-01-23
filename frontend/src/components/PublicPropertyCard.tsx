47d26e36053b6dfba9acf4642c378bcd2a4409d5
kunihiro1200 <tomoko.kunihiro@ifoo-oita.com>
2026-01-24 05:07:45 +0900

    Fix: Use thumbnailUrl property instead of entire image object in PublicPropertyCard

--- frontend/src/components/PublicPropertyCard.tsx
+++ frontend/src/components/PublicPropertyCard.tsx
@@ -98,7 +98,7 @@ const PublicPropertyCard: React.FC<PublicPropertyCardProps> = ({
   };
 
   const thumbnailUrl = property.images && property.images.length > 0
-    ? property.images[0]
+    ? property.images[0].thumbnailUrl
     : '/placeholder-property.jpg';
   
   const typeConfig = getPropertyTypeConfig(property.property_type);
